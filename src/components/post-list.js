490d885737bb599c343f632d9a7485918ff50537
levib00 <levib8@hotmail.com>
2023-04-27 23:25:11 -0400

    Add functions to extend the page or move to the next

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Post } from "./post";
+import { Footer } from "./footer";
 
 export const PostList = (props) => {
+  const [start, setStart] = useState(0);
+  const [numberOfPosts, setNumberOfPosts] = useState(25);
+
+  const extend = () => {
+    // TODO: Inject a div that tells the user the current page above this section of posts
+    setNumberOfPosts(numberOfPosts + 25)
+  }
+  
+  const loadNext = () => {
+    setNumberOfPosts(numberOfPosts + 25)
+    setStart(start + 25)
+  }
+
   return (
     <div>
-      
+      {props.posts.slice(start, numberOfPosts).map(post => <Post key={post.title} title={post.title} />)}
+      <Footer extend={extend} loadNext={loadNext}/>
     </div>
   )
 }
