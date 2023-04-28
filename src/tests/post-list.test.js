312d442edf9e1f1fe323737a1225c9d2153356e6
levib00 <levib8@hotmail.com>
2023-04-27 23:22:56 -0400

    Add tests for extend and load next page buttons

--- src/tests/post-list.test.js
+++ src/tests/post-list.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getByRole, render, screen, act } from "@testing-library/react";
+import {  render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { PostList } from "../components/post-list";
@@ -13,15 +13,63 @@ describe("Test post-list renders properly.", () => {
     return posts
   }
 
-  test('renders the correct number of posts.', () => {
-    render(<PostList posts={generatePosts(25)} />);
+  test('renders all posts if under page limit', () => {
+    const n = 12
+    render(<PostList posts={generatePosts(n)} />);
+    for (let i = 0; i < n; i++) {
+      const post = screen.getByText(`This is a post ${i}`);
+      expect(post).toBeInTheDocument();
+    }
+    const post = screen.queryByText(`This is a post ${n}`);
+    expect(post).toBeNull();
+  });
+
+  test('renders only max page limit even if more posts are provided.', () => {
+    const n = 50
+    render(<PostList posts={generatePosts(n)} />);
     for (let i = 0; i < 25; i++) {
       const post = screen.getByText(`This is a post ${i}`);
       expect(post).toBeInTheDocument();
     }
-    const post = screen.queryByText(`This is a post 25`);
+    const post = screen.queryByText(`This is a post ${n - 1}`);
+    expect(post).toBeNull();
+  });
+  
+  test('pressing appropriate button on footer extends page with more posts.', async() => {
+    const n = 50
+    render(<PostList posts={generatePosts(n)} />);
+    for (let i = 0; i < 25; i++) {
+      const post = screen.getByText(`This is a post ${i}`);
+      expect(post).toBeInTheDocument();
+    }
+    const extend = screen.getByText('load more posts')
+    
+    userEvent.click(extend)
+    
+    for (let i = 0; i < n; i++) {
+      const post = await screen.findByText(`This is a post ${i}`);
+      expect(post).toBeInTheDocument();
+    }
+    const post = screen.queryByText(`This is a post ${n + 1}`);
     expect(post).toBeNull();
   });
 
-  
+  test('pressing appropriate button on footer loads a new page with next posts', async() => {
+    const n = 50
+    render(<PostList posts={generatePosts(n)} />);
+    for (let i = 0; i < 25; i++) {
+      const post = screen.getByText(`This is a post ${i}`);
+      expect(post).toBeInTheDocument();
+    }
+    const loadNext = screen.getByText('load the next page')
+
+    userEvent.click(loadNext)
+
+    for (let i = 25; i < n; i++) {
+      const post = await screen.findByText(`This is a post ${i}`);
+      expect(post).toBeInTheDocument();
+    }
+    const post = screen.queryByText(`This is a post ${1}`);
+    expect(post).toBeNull();
+  });
 })
