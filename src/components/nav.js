0b988e0d44d7a500a61eba2417b81ee25138cd45
levib00 <levib8@hotmail.com>
2023-05-01 22:47:27 -0400

    Enable signing in and displaying username in nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const NavBar = (props) => {
+  const {signIn, signOut, getUserName} = props
+
+  const [username, setUserName] = useState(getUserName()) //might want to optimize by only doing this once and assigning it to state at App.js level
 
   return (
     <div>
       <div>
-        <span>{props.username}</span>
-        <span>logout</span>
+        <span>{username ? username.currentUser.displayName : null}</span>
+        {username ? 
+          <span onClick={async() => {
+              await signOut
+              setUserName(null)
+            }}>Sign out
+          </span>
+          :
+          <span onClick={async() => {
+              await signIn()
+              setUserName(getUserName())
+            }}>Sign in
+          </span>}
       </div>
-      <div>
-        <div className="hero">
-          cloneddit
-        </div>
+      <div className="hero">
+        cloneddit
       </div>
     </div>
   )
