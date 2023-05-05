bfc0b97b2da526ce0cd21070bbe4e8894fe4464d
levib00 <levib8@hotmail.com>
2023-05-04 20:07:43 -0400

    Add links to submit pages

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getFirestore, getDocs, doc, setDoc, collection } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { Link } from "react-router-dom";
+
 
 export const SideBar = () => {
-
+  
   return (
     <div>
       <h2>all</h2>
       <div>
         displaying content from all topics.
       </div>
-      <button>Submit a new link</button>
-      <button>Submit a new text post</button>
+      <Link to='/submit/submit-link'><button>Submit a new link</button></Link>
+      <Link to='/submit/submit-text'><button>Submit a new text post</button></Link>
     </div>
   )
 }
