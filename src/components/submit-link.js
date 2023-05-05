a594c54456b8cba28f1c47eb776a6bcd71da66e9
levib00 <levib8@hotmail.com>
2023-05-04 20:14:45 -0400

    Submit post info.

--- src/components/submit-link.js
+++ src/components/submit-link.js
@@ -1,28 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { SubmitPage } from "./submission-page";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from 'uuid'
 
-export const SubmitLink = () => {
+export const SubmitLink = (props) => {
+  const {db} = props
+
+  const [selectedFile, setSelectedFile] = useState(undefined);
+  const [titleInput, setTitleInput] = useState('')
+  const [topicInput, setTopicInput] = useState('')
+
+  async function handleFiles(files) {
+    const file = files;
+    
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+      setSelectedFile(e.target.result)
+    }
+  }
+  
+
+  const submitPosts = async() => {
+      await setDoc(doc(db, 'posts', uuidv4()), {
+        img: selectedFile,
+        karma: 0,
+        timeStamp: serverTimestamp(),
+        title: titleInput,
+        topic: topicInput,
+        userId: 'userid' //getAuth to put username here.
+      });
+  }
+
   return (
     <div>
+      <SubmitPage />
       <div>
         <label>url</label>
         <input type="text"></input>
       </div>
       <div>
         <label>image/video</label>
-        <input type="file"></input>
+        <input
+         type="file" accept="video/*, image/*"
+         onChange={(e) => {
+          handleFiles(e.target.files[0])
+          setSelectedFile(e.target.files[0])
+        }}
+        />
       </div>
       <div>
         <label>title</label>
-        <textarea></textarea>
+        <textarea onChange={(e) => {setTitleInput(e.target.value)}} value={titleInput}></textarea>
       </div>
       <div>
         <label>topic</label>
-        <input type="text"></input>
+        <input type="text" onChange={(e) => {setTopicInput(e.target.value)}} value={topicInput}></input>
       </div>
       <div>
         anything is saved and is subject to be removed at any time for any reason.
       </div>
-      <button>Submit</button>
+      <button onClick={() => {
+        submitPosts()
+      }}>Submit</button>
     </div>
   )
 }
