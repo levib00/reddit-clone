bdac13aaff33be11107b0b72adc3ed3c603db091
levib00 <levib8@hotmail.com>
2023-05-04 20:09:04 -0400

    Add links to post submission pages

--- src/components/submission-page.js
+++ src/components/submission-page.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { SubmitLink } from "./submit-link";
+import { SubmitText } from "./submit-text";
 
 export const SubmitPage = () => {
   const [isLink, setIsLink] = useState(false)
   return (
     <div>
-      <button>link</button>
-      <button>text</button>
+      <Link to='/submit/submit-link'><button>link</button></Link>
+      <Link to='/submit/submit-text'><button>text</button></Link>
     </div>
   )
 }
