57076fbf1057545bbbc82686ac0326e76e72d241
levib00 <levib8@hotmail.com>
2023-05-11 00:07:19 -0400

    Add function to get and load top level comments

--- src/components/link-post-page.js
+++ src/components/link-post-page.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SubmitComment } from "./commentSubmission";
 import { Comment } from "./comment";
+import { useParams } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import {v4 as uuidv4} from 'uuid'
 
-export const postPage = () => {
+export const LinkPostPage = ({ db }) => {
+  const { postId } = useParams()
+  const [comments, setComments] = useState(null)
+  const [colPath] = useState([db, 'posts', postId, 'comments'])
+
+  useEffect(() => {
+    const getComments = async() => { 
+      // Gets coordinates for character chosen from dropdown.
+      const commentCollection = collection(db, 'posts', postId, 'comments');
+      const commentSnapshot = await getDocs(commentCollection);
+      const commentArr = [];
+      commentSnapshot.docs.forEach(async doc => {
+        const contents = doc.data()
+        commentArr.push(contents)
+      })
+      return commentArr
+    }
+
+    const characterSetter = async() => {
+      try {
+        setComments(await getComments())
+      } catch(error) {
+        console.error(error)
+      }
+    }
+    characterSetter()
+  }, [db, postId])
 
   return (
     <div>
       <div>
-        <img>scaled down image</img>
+        <img alt="scaled down image"/>
       </div>
       <div>
         <div>title</div>
@@ -17,11 +46,11 @@ export const postPage = () => {
           <div>user</div>
         </div>
         <div>
-          <img>scaled up image that can be collapsed by a button</img>
+          <img alt="scaled up image that can be collapsed by a button"/>
         </div>
       </div>
       <SubmitComment />
-      <Comment />
+      {comments ? comments.map(comment => <Comment key={uuidv4()} comment={ comment } db={db} prev={colPath}/>) : null}
     </div>
   )
 }
