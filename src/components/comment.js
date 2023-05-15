a3d08297f2e68e161ec2eb2c20fcf50d4569d462
levib00 <levib8@hotmail.com>
2023-05-15 16:57:48 -0400

    Allow long comment chains to be expanded

--- src/components/comment.js
+++ src/components/comment.js
@@ -3,12 +3,19 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import {v4 as uuidv4} from 'uuid'
 
-export const Comment = ({ comment, prev }) => {
+export const Comment = ({ comment, prev, level, setTopComments, setColPath }) => {
   const { postId } = useParams()
   const [comments, setComments] = useState()
   const [prevParams, setPrevParams] = useState(prev)
 
+  const getTopComments = async(prevs) => { 
+    setColPath(prevs)
+  }
+
   useEffect(() => {
+    if (!comment.commentId) {
+      return
+    }
     const getComments = async() => { 
       const newParams = [...prevParams, comment.commentId, 'replies']
       setPrevParams(newParams)
@@ -23,21 +30,22 @@ export const Comment = ({ comment, prev }) => {
       return commentArr
     }
 
-    const characterSetter = async() => {
+    const characterSetter = async() => { //rename
       try {
-        setComments(await getComments())
+        setComments(await getComments())        
       } catch(error) {
         console.error(error)
       }
     }
     characterSetter()
   }, [postId, setPrevParams, comment.commentId, prev])
-
   return (
     <div>
       <div>{comment.username}</div> <div>{comment.timeStamp}</div>
       <div>{comment.content}</div>
-      {comments ? comments.map(comment => <Comment key={uuidv4()} comment={comment} prev={prevParams} />) : null}
+      {level < 10 ? (comments && comments.length > 0 ? comments.map(comment => <Comment key={uuidv4()} setColPath={setColPath} setTopComments={setTopComments} level={level + 1} comment={comment} prev={prevParams} />) : null)
+      :
+      <button onClick={() => getTopComments(prevParams)}>Continue this thread</button>}
     </div>
   )
 }
