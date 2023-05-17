a8f2194971ef13b4c696d941e33f010158641017
levib00 <levib8@hotmail.com>
2023-05-17 18:05:42 -0400

    Add functionality to reply to comments.

--- src/components/comment.js
+++ src/components/comment.js
@@ -2,11 +2,14 @@ import { collection, getDocs } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import {v4 as uuidv4} from 'uuid'
+import { SubmitComment } from "./submit-comment";
 
-export const Comment = ({ comment, prev, level, setTopComments, setColPath }) => {
+export const Comment = ({ comment, prev, level, setTopComments, setColPath, db, getUserName, signInWithPopup }) => {
   const { postId } = useParams()
   const [comments, setComments] = useState()
   const [prevParams, setPrevParams] = useState(prev)
+  const [showReplyBox, setShowReplyBox] = useState(false)
+  const [date] = useState(comment.timeStamp ? new Date(comment.timeStamp.seconds*1000).toString() : null) // * can remove ternary in finish, but test posts dont have dates.
 
   const getTopComments = async(prevs) => { 
     setColPath(prevs)
@@ -19,7 +22,6 @@ export const Comment = ({ comment, prev, level, setTopComments, setColPath }) =>
     const getComments = async() => { 
       const newParams = [...prevParams, comment.commentId, 'replies']
       setPrevParams(newParams)
-      // Gets coordinates for character chosen from dropdown.
       const commentCollection = collection.apply(null, newParams);
       const commentSnapshot = await getDocs(commentCollection);
       const commentArr = [];
@@ -41,9 +43,11 @@ export const Comment = ({ comment, prev, level, setTopComments, setColPath }) =>
   }, [postId, setPrevParams, comment.commentId, prev])
   return (
     <div>
-      <div>{comment.username}</div> <div>{comment.timeStamp}</div>
+      <div>{comment.username}</div> <div>{date}</div>
       <div>{comment.content}</div>
-      {level < 10 ? (comments && comments.length > 0 ? comments.map(comment => <Comment key={uuidv4()} setColPath={setColPath} setTopComments={setTopComments} level={level + 1} comment={comment} prev={prevParams} />) : null)
+      <div onClick={() => setShowReplyBox(true)}>reply</div>
+      {showReplyBox ? <SubmitComment getUserName={getUserName} signInWithPopup={signInWithPopup} dbPath={prevParams} /> : null}
+      {level < 10 ? (comments && comments.length > 0 ? comments.map(comment => <Comment key={uuidv4()} getUserName={getUserName} setColPath={setColPath} setTopComments={setTopComments} level={level + 1} comment={comment} prev={prevParams} />) : null)
       :
       <button onClick={() => getTopComments(prevParams)}>Continue this thread</button>}
     </div>
--- src/components/submit-comment.js
+++ src/components/submit-comment.js
@@ -3,22 +3,21 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid'
 import { SignInModal } from "./sign-in-prompt";
 
-export const SubmitComment = ({getUserName, db, postId, isReply, signInWithPopup}) => {
-  const [commentInput, setCommentInput] = useState()
+export const SubmitComment = ({getUserName, dbPath, signInWithPopup}) => {
+  const [commentInput, setCommentInput] = useState('')
   const [showSignIn, setShowSignIn] = useState(false)
 
   const submitPosts = async() => {
+    const commentId = uuidv4()
     const username = await getUserName().currentUser.displayName
-    const dbPath = [db, 'posts', postId]
     // if (isReply) {dbPath.concat([pathToComment])
     if (username !== null) {
-      await setDoc(doc.apply(dbPath), {
-        parentId: null,
-        text: commentInput,
+      await setDoc(doc.apply(null, dbPath.concat(commentId)), {
+        content: commentInput,
         karma: 0,
         timeStamp: serverTimestamp(),
         userName: username,
-        commentId: uuidv4(),
+        commentId: commentId,
       });
     } else {
       setShowSignIn(true)
@@ -28,7 +27,7 @@ export const SubmitComment = ({getUserName, db, postId, isReply, signInWithPopup
   return (
     <div>
       {showSignIn ? <SignInModal setShowSignIn={setShowSignIn} signInWithPopup={signInWithPopup} from={'submit a comment'} getUserName={getUserName} /> : null}
-      <textarea onClick={(e) => setCommentInput(e.target.value)} value={commentInput}></textarea>
+      <textarea onChange={(e) => setCommentInput(e.target.value)} value={commentInput}></textarea>
       <button onClick={submitPosts}>save</button>
     </div>
   )
