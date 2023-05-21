b585c40ac5284e827a82d6b2ae6afe9b5ca19b38
levib00 <levib8@hotmail.com>
2023-05-21 17:19:21 -0400

    Update to working add page test

--- src/tests/post-list.test.js
+++ src/tests/post-list.test.js
@@ -1,21 +1,37 @@
 import React from "react";
+import {v4 as uuidv4} from 'uuid'
 import { waitFor, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { PostList } from "../components/post-list";
+import {MemoryRouter} from 'react-router-dom';
+import Router from 'react-router';
 
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useParams: jest.fn(),
+}));
 describe("Test post-list renders properly.", () => {
   const generatePosts = (n) => {
     const posts = [];
     for (let i = 0; i < n; i++) {
-      posts.push({'title' : `This is a post ${i}`})
+      posts.push({title : `This is a post ${i}`})
     }
     return posts
   }
 
   test('renders all posts if under page limit', () => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ topic: 'all' });
+  
     const n = 12
-    render(<PostList posts={generatePosts(n)} />);
+    const setTopic = jest.fn()
+    const postSetter = jest.fn()
+    
+    render(
+      <MemoryRouter>
+        <PostList key={uuidv4()} posts={generatePosts(n)}  setTopic={setTopic} postSetter={postSetter}/>
+      </MemoryRouter>
+    );
     for (let i = 0; i < n; i++) {
       const post = screen.getByText(`This is a post ${i}`);
       expect(post).toBeInTheDocument();
@@ -25,8 +41,18 @@ describe("Test post-list renders properly.", () => {
   });
 
   test('renders only max page limit even if more posts are provided.', () => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ topic: 'all' });
+  
     const n = 50
-    render(<PostList posts={generatePosts(n)} />);
+    const setTopic = jest.fn()
+    const postSetter = jest.fn()
+    
+    render(
+      <MemoryRouter>
+        <PostList key={uuidv4()} posts={generatePosts(n)}  setTopic={setTopic} postSetter={postSetter}/>
+      </MemoryRouter>
+    );
+    
     for (let i = 0; i < 25; i++) {
       const post = screen.getByText(`This is a post ${i}`);
       expect(post).toBeInTheDocument();
@@ -36,8 +62,18 @@ describe("Test post-list renders properly.", () => {
   });
   
   test('pressing appropriate button on footer extends page with more posts.', async() => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ topic: 'all' });
+  
     const n = 50
-    render(<PostList posts={generatePosts(n)} />);
+    const setTopic = jest.fn()
+    const postSetter = jest.fn()
+    
+    render(
+      <MemoryRouter>
+        <PostList key={uuidv4()} posts={generatePosts(n)}  setTopic={setTopic} postSetter={postSetter}/>
+      </MemoryRouter>
+    );
+
     for (let i = 0; i < 25; i++) {
       const post = screen.getByText(`This is a post ${i}`);
       expect(post).toBeInTheDocument();
@@ -46,14 +82,23 @@ describe("Test post-list renders properly.", () => {
     
     userEvent.click(extend)
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < 49; i++) {
       expect(await screen.findByText(`This is a post ${i}`)).toBeInTheDocument();
     }
   });
 
   test('pressing appropriate button on footer loads a new page with next posts', async() => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ topic: 'all' });
+  
     const n = 50
-    render(<PostList posts={generatePosts(n)} />);
+    const setTopic = jest.fn()
+    const postSetter = jest.fn()
+    
+    render(
+      <MemoryRouter>
+        <PostList key={uuidv4()} posts={generatePosts(n)}  setTopic={setTopic} postSetter={postSetter}/>
+      </MemoryRouter>
+    );
     for (let i = 0; i < 25; i++) {
       const post = await screen.findByText(`This is a post ${i}`);
       expect(post).toBeInTheDocument();
@@ -70,5 +115,31 @@ describe("Test post-list renders properly.", () => {
     await waitFor(() => {
       expect(post).not.toBeInTheDocument();
     });
+  });
+
+  test('page numbers show when extending page', async() => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ topic: 'all' });
+  
+    const n = 75
+    const setTopic = jest.fn()
+    const postSetter = jest.fn()
+    
+    render(
+      <MemoryRouter>
+        <PostList key={uuidv4()} posts={generatePosts(n)}  setTopic={setTopic} postSetter={postSetter}/>
+      </MemoryRouter>
+    );
+
+    for (let i = 0; i < 25; i++) {
+      const post = screen.getByText(`This is a post ${i}`);
+      expect(post).toBeInTheDocument();
+    }
+    const extend = screen.getByText('load more posts')
+    
+
+    for (let i = 0; i < 2; i++) {
+      userEvent.click(extend)
+      expect(await screen.findByText(`Page ${i}`)).toBeInTheDocument();
+    }
   });
 })
