63f2fac1b519c8b94d1d9fd90e6d83a7f87209b7
levib00 <levib8@hotmail.com>
2023-05-21 17:18:53 -0400

    Update test to working again

--- src/tests/nav.test.js
+++ src/tests/nav.test.js
@@ -3,11 +3,28 @@ import { getByRole, render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { NavBar } from "../components/nav";
+import {MemoryRouter} from 'react-router-dom';
+import Router from 'react-router';
 
 
 describe("Test nav bar renders properly", () => {
+
+  const getUserName = () => {
+    const currentUser = {currentUser:{displayName : 'Username'}}
+    return currentUser
+  }
+
+  const signOut = jest.fn()
+  const signIn = jest.fn()
+  const topic = 'topic'
+
   test('Nav bar renders passed username', () => {
-    render(<NavBar username={'Username'} />);
+    
+    render(
+      <MemoryRouter>
+        <NavBar getUserName={getUserName} signIn={signIn} signOut={signOut} topic={topic} />
+      </MemoryRouter>
+    );
     const username = screen.getByText('Username');
     expect(username).toBeInTheDocument();
   });
