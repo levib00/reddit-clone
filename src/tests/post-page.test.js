c839cc775c8435b77f57758b10bc14b27bba3677
levib00 <levib8@hotmail.com>
2023-05-21 17:19:48 -0400

    initial commit

---
+++ src/tests/post-page.test.js
@@ -0,0 +1,8 @@
+//TODO: test post page, and comments maybe.
+import { render, screen } from '@testing-library/react';
+import React, { useState } from 'react';
+import App from '../App';
+
+test.skip('renders learn react link', () => {
+  render(<App />);
+});
---
+++ src/tests/submit-page.test.js
@@ -0,0 +1,8 @@
+//TODO: test SignInPrompt pops if auth isn't provided.
+import { render, screen } from '@testing-library/react';
+import React, { useState } from 'react';
+import App from '../App';
+
+test.skip('renders learn react link', () => {
+  render(<App />);
+});
