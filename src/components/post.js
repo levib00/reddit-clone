88bd2bdbe5520114b0303aa44205ee61d88893a6
levib00 <levib8@hotmail.com>
2023-05-23 21:59:03 -0400

    Add up/downvote for posts on postlist

--- src/components/post.js
+++ src/components/post.js
@@ -1,14 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import expandText from '../assets/text-image.png'
+import collapse from '../assets/collapse.png';
+import expandImage from '../assets/expand-img.png';
+import { doc, updateDoc } from "firebase/firestore";
 
-export const Post = ({post}) => {
-  const {img, karma, title, text, topic, timestamp, userId, id} = post
+export const Post = ({post, username, db}) => {
+  const {img, karma, title, text, topic, timestamp, userId, id, upped, downed} = post
   
   const [isImage, setIsImage] = useState(null)
+  const [expanded, setExpanded] = useState(false)
+  const [isUpped, setIsUpped] = useState(upped.includes(username.currentUser.uid))
+  const [isDowned, setIsDowned] = useState(downed.includes(username.currentUser.uid))
 
   useEffect(() => {
     setIsImage(img ? true : false)
   }, [img])
+
+  const updateVote = async(voteArr, vote, otherArr, otherVote) => {
+    if (username.currentUser) {
+      const postUpdate = doc(db, 'posts', id)
+      const index = vote.indexOf(username.currentUser.uid)
+      const otherIndex = otherVote.indexOf(username.currentUser.uid)
+      if (index < 0) {
+        await updateDoc(postUpdate, {
+          [voteArr]: [...vote, username.currentUser.uid]
+        })
+      } else {
+        const newVote = [...vote]
+        newVote.splice(index, 1)
+        await updateDoc(postUpdate, {
+          [voteArr]: newVote
+        })
+      }
+      if (otherIndex < 0) {
+        const newVote = [...otherVote]
+        newVote.splice(otherIndex, 1)
+        await updateDoc(postUpdate, {
+          [otherArr]: newVote
+        })
+      }
+    } else {
+      //TODO: sign in prompt here
+    }
+  }
+
+  const handleVote = (voteArr, vote) => {
+    updateVote(voteArr, vote)
+  }
 
   return (
     <div>
@@ -16,11 +55,16 @@ export const Post = ({post}) => {
       { isImage ? <img src={img} alt={`${title}`}/> : <div>{text}</div>}
       <div>
         <div>
+          <button onClick={() => handleVote('upped', upped,'downed', downed)}>{isUpped ? 'upped' : 'not upped'}</button>
+          <p>{karma}</p>
+          <button onClick={() => handleVote('downed', downed, 'upped', upped)}>{isDowned ? 'downed' : 'not downed'}</button>
+        </div>
+        <div>
           <div><Link to={`/topic/${topic}`}>{topic}</Link></div>
           <div>{title}</div>
         </div>
         <div>
-          <button>expand/collapse button</button>
+          <button onClick={() => setExpanded(!expanded)}>{expanded ? <img src={collapse}></img> : isImage ? <img src={expandImage}></img> : <img src={expandText}></img>}</button>
           <div>
             <div>
               {timestamp}
@@ -29,6 +73,7 @@ export const Post = ({post}) => {
             <div>
               { isImage ? <Link to={`/post/link/${id}`}>view comments</Link> : <Link to={`/text/${id}`}>view comments</Link>}
             </div>
+            { expanded ? isImage ? <img src={img} alt={`${title}`}/> : <div>{text}</div> : null  }
           </div>
         </div>
       </div>
