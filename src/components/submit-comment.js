451a42b372f9f06beabe9a6feaae1ffbb3d56139
levib00 <levib8@hotmail.com>
2023-05-30 19:38:22 -0400

    Fix bugs caused by different paths/improve UX

--- src/components/submit-comment.js
+++ src/components/submit-comment.js
@@ -3,32 +3,50 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid'
 import { SignInModal } from "./sign-in-prompt";
 
-export const SubmitComment = ({getUserName, dbPath, signInWithPopup, setShowReplyBox, showReplyBox}) => {
+export const SubmitComment = ({getUserName, dbPath, signInWithPopup, setShowReplyBox, showReplyBox = null, comments, setComments}) => {
   const [commentInput, setCommentInput] = useState('')
   const [showSignIn, setShowSignIn] = useState(false)
 
-  const submitComment = async() => {
-    const commentId = uuidv4()
-    const username = await getUserName().currentUser.displayName
+  const submitComment = async(newComment, username, commentId) => {
     if (username !== null) {
-      await setDoc(doc.apply(null, dbPath.concat(commentId)), {
-        content: commentInput,
-        karma: 0,
-        timeStamp: serverTimestamp(),
-        userName: username,
-        commentId: commentId,
+      await setDoc(doc.apply(null, dbPath.concat([commentId])), {
+        ...newComment
       });
     } else {
       setShowSignIn(true)
     }
-    setShowReplyBox(!showReplyBox)
+    if (showReplyBox !== null) {
+      setShowReplyBox(!showReplyBox)
+    }
+  }
+
+  const clearBox = () => {
+    setCommentInput('')
+  }
+
+  const handleSubmit = async () => {
+    const commentId = uuidv4()
+    const username = await getUserName().currentUser.displayName
+    const newComment = {
+      content: commentInput,
+      karma: 0,
+      timeStamp: serverTimestamp(),
+      username: username,
+      commentId: commentId,
+      upped: [],
+      downed: [],
+    }
+    console.log(serverTimestamp())
+    submitComment(newComment, username, commentId);
+    setComments([...comments, newComment])
+    clearBox();
   }
 
   return (
     <div>
       {showSignIn ? <SignInModal setShowSignIn={setShowSignIn} signInWithPopup={signInWithPopup} from={'submit a comment'} getUserName={getUserName} /> : null}
       <textarea onChange={(e) => setCommentInput(e.target.value)} value={commentInput}></textarea>
-      <button onClick={submitComment}>save</button>
+      <button onClick={handleSubmit}>save</button>
     </div>
   )
 }
