39bd1ac47ca723110351a7d98bfcb272088c3821
levib00 <levib8@hotmail.com>
2023-06-04 00:40:59 -0400

    Add saving posts functionality

--- src/components/post.js
+++ src/components/post.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import expandText from '../assets/text-image.png'
 import collapse from '../assets/collapse.png';
 import expandImage from '../assets/expand-img.png';
-import { doc, updateDoc } from "firebase/firestore";
+import { DocumentReference, FieldValue, arrayRemove, arrayUnion, doc, getDoc, query, setDoc, updateDoc } from "firebase/firestore";
 import { SignInModal } from "./sign-in-prompt";
 
 export const Post = ({post, db, getUserName, signInWithPopup, from}) => {
-  const {img, title, text, topic, timeStamp, userId, id, upped, downed} = post
+  const {img, title, text, topic, timeStamp, userId, id, upped, downed, saved} = post
   
   const [username] = useState(getUserName())
   const [isImage, setIsImage] = useState(null)
@@ -15,6 +15,7 @@ export const Post = ({post, db, getUserName, signInWithPopup, from}) => {
   const [expanded, setExpanded] = useState(from === 'post-page' ? true : false)
   const [isUpped, setIsUpped] = useState(upped.includes(username.currentUser.uid))
   const [isDowned, setIsDowned] = useState(downed.includes(username.currentUser.uid))
+  const [isSaved, setIsSaved] = useState(saved.includes(username.currentUser.uid))
   const [showSignIn,setShowSignIn] = useState(null)
   
   useEffect(() => {
@@ -22,6 +23,7 @@ export const Post = ({post, db, getUserName, signInWithPopup, from}) => {
   }, [img])
 
   useEffect(() => {
+    setIsSaved(thisPost.saved ? thisPost.saved.includes(username.currentUser.uid) : false)
     setIsUpped(thisPost.upped ? thisPost.upped.includes(username.currentUser.uid) : false)
     setIsDowned(thisPost.downed ? thisPost.downed.includes(username.currentUser.uid) : false)
   }, [thisPost, username.currentUser.uid])
@@ -34,7 +36,7 @@ export const Post = ({post, db, getUserName, signInWithPopup, from}) => {
     if (newOtherVote !== null) {
       clone[otherArr] = newOtherVote
     }
-    clone.karma = clone.upped.length - clone.downed.length
+    clone.karma = clone.upped.length - clone.downed.length // change to happen conditionally
     setThisPost(clone)
   }
 
@@ -73,8 +75,53 @@ export const Post = ({post, db, getUserName, signInWithPopup, from}) => {
     }
   }
 
+  const updateSaves = async(voteArr, vote, newParams) => { // * this needs to be refactored and simplified.
+    if (username.currentUser) {
+      const postUpdate = await doc.apply(null, newParams)
+      const index = vote.indexOf(username.currentUser.uid)
+      const newVote = [...vote]
+      console.log(index)
+      if (index < 0) {
+        newVote.push(username.currentUser.uid)
+        updatePosts(newVote, null, voteArr, thisPost)
+      } else {
+        newVote.splice(index, 1)
+        updatePosts(newVote, null, voteArr, thisPost)
+      }
+      await updateDoc(postUpdate, {
+        [voteArr]: newVote,
+      });
+    } else {
+      //setShowSignIn(true)
+    }
+  }
+
   const handleVote = async(voteArr, vote, otherArr, otherVote) => {
-    updateVote(voteArr, vote, otherArr, otherVote, [db, 'posts', id])
+    updateVote(voteArr, vote, otherArr, otherVote, [db, 'posts', id]) // hardcode path
+  }
+
+  const savePost = async() => {
+    const q = doc(db, "saved", username.currentUser.uid)
+    await updateDoc(q, {
+      savedPosts: arrayUnion(doc(db, 'posts', thisPost.id))
+    });
+  }
+
+  const unsavePost = async() => {
+    const q = doc(db, "saved", username.currentUser.uid)
+    await updateDoc(q, { // change to use uid maybe
+      savedPosts: arrayRemove(doc(db, 'posts', thisPost.id))
+    });
+  }
+
+  const handleSave = (arrName, arr) => {
+    savePost()
+    updateSaves(arrName, arr, [db, 'posts', id])
+  }
+
+  const handleUnsave = (arrName, arr) => {
+    unsavePost()
+    updateSaves(arrName, arr, [db, 'posts', id])
   }
 
   return (
@@ -101,6 +148,13 @@ export const Post = ({post, db, getUserName, signInWithPopup, from}) => {
             <div>
               <Link to={`/post/link/${id}`}>view comments</Link>
             </div>
+            {!isSaved ? <div onClick={() => handleSave('saved', thisPost.saved)}>
+              save
+            </div> 
+            :
+            <div onClick={() => handleUnsave('saved', thisPost.saved)}>
+              unsave
+            </div>}
             { expanded ? isImage ? <img src={img} alt={`${title}`}/> : <div>{text}</div> : null  }
           </div>
         </div>
