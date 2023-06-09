0a9c3d1180b7f40cc3ee30a4b86b8a5cbb7ece00
levib00 <levib8@hotmail.com>
2023-06-09 01:24:48 -0400

    Fix bugs with showing username & sign in/out

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const NavBar = (props) => {
-  const {signIn, signOut, getUserName, topic} = props
+  const {signIn, signOut, getUserName, topic, isUserSignedIn} = props
 
   const [username, setUserName] = useState(getUserName())
 
   return (
     <div>
       <div>
-        <span>{username ? username.currentUser !== null ? username.currentUser.displayName : null : null}</span>
+        <span>{isUserSignedIn && username ? username.currentUser.displayName : null}</span>
         {topic ? topic : null }
-        {username ? 
+        {isUserSignedIn ? 
           <span onClick={async() => {
               await signOut()
               setUserName(null)
