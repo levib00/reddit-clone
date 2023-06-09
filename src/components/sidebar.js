2a5b2bc0d17a49cc9f41fda8a5398f52d4c890d0
levib00 <levib8@hotmail.com>
2023-06-09 01:25:14 -0400

    Add search bar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { getFirestore, getDocs, doc, setDoc, collection } from 'firebase/firestore'
-import { initializeApp } from "firebase/app";
+import react, { useState } from "react";
 import { Link } from "react-router-dom";
 
 
 export const SideBar = ({ topic }) => {
-  
+  const [ searchQuery, setSearchQuery ] = useState('')
+
   return (
     <div>
+      <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+      <Link to={`/search/${searchQuery}`}><button>search</button></Link>
       <h2>{topic || 'all'}</h2>
       <div>
         displaying content {topic ? `on the topic of ${topic}` : 'from all topics'}.
