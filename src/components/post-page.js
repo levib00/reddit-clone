700e26031ddf53fe53e9e213f7e9bc2328bfea69
levib00 <levib8@hotmail.com>
2023-06-10 21:38:55 -0400

    Add comments and change name of component and file.

--- src/components/post-page.js
+++ src/components/post-page.js
@@ -7,7 +7,7 @@ import {v4 as uuidv4} from 'uuid'
 import { SideBar } from "./sidebar";
 import { Post } from "./post";
 
-export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
+export const PostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
   const { postId } = useParams()
 
   const [post, setPost] = useState(null)
@@ -17,18 +17,21 @@ export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
   const [sortOption, setSortOption] = useState(sortingOptions[0])
   const [showDropDown, setShowDropDown] = useState(false)
 
+  // Set the topic to the posts corresponding topic
   useEffect(() => {
     if (post) {
       setTopic(post.topic)
     }
   }, [post, setTopic])
 
+  // Set topic to all when leaving post page
   useEffect(() => {
     return () => {
       setTopic('all')
     }
   }, [setTopic])
 
+  // Sort the comments based on the selected sorting option
   const sortComments = (posts, sortOption) => {
     posts.sort((a, b) => {
       if (b[sortOption] < a[sortOption]) {
@@ -42,6 +45,7 @@ export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
     return posts
   }
 
+  // Rerender comments in sorted order when sort option changes
   useEffect(() => {
     if (comments) {
       const commentsClone = [...comments]
@@ -49,18 +53,22 @@ export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
     }
   }, [sortOption.option])
 
+  // Handle the selection of a sorting option
   const handleSortSelect = (option) => {
     setSortOption(option)
     setShowDropDown(!showDropDown)
   }
 
+  // Gets and sets current post from database on page load
   useEffect(() => {
+    // Retrieve the post data from the database
     const getPost = async() => { 
       const postRef = doc(db, 'posts', postId);
       const postSnapshot = await getDoc(postRef);
       const contents = postSnapshot.data()
       return contents
     }
+
     const postSetter = async() => {
       try {
         setPost(await getPost())
@@ -68,9 +76,11 @@ export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
         console.error(error)
       }
     }
+
     postSetter()
   }, [db])
 
+  // Gets top level comments from database (nested comments will be gotten in comment component)
   const getComments = async() => { 
     const commentCollection = collection.apply(null, colPath)
     const commentSnapshot = await getDocs(commentCollection);
@@ -82,6 +92,7 @@ export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
     return commentArr
   }
 
+  // Sets top level comments in state to be rendered
   useEffect(() => {
     const commentSetter = async() => {
       try {
@@ -96,27 +107,29 @@ export const LinkPostPage = ({ db, getUserName, signIn, setTopic, posts }) => {
 
   return (
     <div>
-      {
-      post ?
-       <>
-        <SideBar topic={post.topic} />
-        <Post key={uuidv4()} signInWithPopup={signIn} posts={posts} setPosts={setPost} db={db} getUserName={getUserName} post={post} from={'post-page'} />
-      </>
-      :
-      null
+      {post ?
+        <>
+          <SideBar topic={post.topic} />
+          <Post key={uuidv4()} signInWithPopup={signIn} posts={posts} setPosts={setPost} db={db} getUserName={getUserName} post={post} from={'post-page'} />
+        </>
+        : null
       }
+      {/* Submit Comment form */}
       <SubmitComment getUserName={getUserName} signInWithPopup={signIn} dbPath={[db, 'posts', postId, 'comments']} postId={postId} db={db} comments={comments} setComments={setComments} />
       <div>
+        {/* Sort dropdown */}
         <div onClick={() => setShowDropDown(!showDropDown)}>
           {sortOption.displayed}
-          </div>
-          {showDropDown ? 
-           <div>
-            {sortingOptions.map(option => sortOption.option === option.option ? null : <div key={uuidv4()} onClick={() => handleSortSelect(option)}>{option.displayed}</div>)}
-           </div> 
-           : 
-          null}
         </div>
+        {/* Sorting options (only shows options that aren't already selected) */}
+        {showDropDown ? 
+          <div>
+            {sortingOptions.map(option => sortOption.option === option.option ? null : <div key={uuidv4()} onClick={() => handleSortSelect(option)}>{option.displayed}</div>)}
+          </div> 
+          : null
+        }
+        </div>
+        {/* Render comments */}
       {comments && comments.length > 0 ? comments.map(comment => <Comment key={uuidv4()} getComments={getComments} setColPath={setColPath} setTopComments={setComments} level={0} getUserName={getUserName} postId={postId} signInWithPopup={signIn} comment={comment} db={db} prev={colPath}/>) : null}
     </div>
   )
