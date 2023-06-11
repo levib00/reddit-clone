a54dacb84549c7ba765229547cc1715d62fe72de
levib00 <levib8@hotmail.com>
2023-06-10 21:10:24 -0400

    Add comments

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,15 @@ export const Footer = (props) => {
 
   return (
     <div>
-      <div><button onClick={extend}>load more posts</button></div> or <div> <button onClick={loadNext}>load the next page</button></div> 
+      <div>
+        {/* "Load more posts" button */}
+        <button onClick={extend}>load more posts</button>
+      </div>
+      or 
+      <div>
+        {/* "Load the next page" button */}
+        <button onClick={loadNext}>load the next page</button>
+      </div> 
     </div>
   )
 }
